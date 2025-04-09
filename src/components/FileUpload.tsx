d3a4c35e56e830a1e00faf82d1bacc519523e317
riccc787 <rrbarrancob@gmail.com>
2025-04-08 20:54:22 -0500

    Delete src/components directory

--- src/components/FileUpload.tsx
+++
@@ -1,7 +0,0 @@
-export const FileUpload = ({ onFileSelect }) => (
-  <input 
-    type="file" 
-    onChange={(e) => e.target.files?.[0]?.text().then(onFileSelect)}
-    className="mb-4 text-white"
-  />
-);
--- src/components/HistoryItem.tsx
+++
@@ -1,5 +0,0 @@
-export const HistoryItem = ({ item }) => (
-  <div className="p-2 bg-gray-700 rounded mb-2 cursor-pointer hover:bg-gray-600">
-    {item.prompt.substring(0, 50)}...
-  </div>
-);
